ec6f7a5fb87665664a4d2ea477d09aaf4bb21b2d
Federico Carrizo <carrizofg@gmail.com>
2019-02-24 22:17:17 -0300

    leetcode-11-container-with-most-water.js

---
+++ leetcode-11-container-with-most-water.js
@@ -0,0 +1,40 @@
+/*
+    # Federico Carrizo
+    # carrizofg@gmail.com
+    # https://github.com/justfede
+
+    11. Container With Most Water
+
+    Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+    Note: You may not slant the container and n is at least 2.
+
+    Example:
+        Input: [1,8,6,2,5,4,8,3,7]
+        Output: 49
+
+    Solution Big-O: 
+        Time Complexity: O(n)
+
+*/
+
+let input = [1,8,6,2,5,4,8,3,7];
+
+const solution = (input) => {
+    let max     = 0;
+    let left    = 0;
+    let right   = input.length - 1;
+    
+    while (left < right) {
+        max = Math.max(max, Math.min(input[left], input[right]) * (right - left));
+        if (input[left] < input[right]){
+                left++;
+        }else{
+            right--;
+        }
+                
+    }
+    
+    return max;
+}
+
+console.log(solution(input));
